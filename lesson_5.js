15db2c3c21513caaeebde392e4efae787d5d73b4
Micah Bales <micahbales@gmail.com>
2016-08-15 12:37:54 -0400

    completed lesson 5

--- lesson_5.js
+++ lesson_5.js
@@ -1,14 +1,16 @@
 var fs = require('fs');
-var path = require('path');
+  var path = require('path');
 
-var folder = process.argv[2];
-var ext = '.' + process.argv[3];
+  var folder = process.argv[2];
+  var ext = '.' + process.argv[3];
 
-fs.readdir(folder, function (err, list) {
+  fs.readdir(folder, function (err, list) {
+    if (err) return console.error(err);
 
-  for (i = 0; i < list.length; i++)
-  if (path.extname(list[i]) === ext) {
-    console.log(list[i]);
-  }
+    for (i = 0; i < list.length; i++)
 
-});
+      if (path.extname(list[i]) === ext) {
+        console.log(list[i]);
+      }
+
+  });
