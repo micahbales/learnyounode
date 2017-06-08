7760f2914103f561dd30ee754cb4f7a1a408f690
Micah Bales <micahbales@gmail.com>
2017-06-08 12:00:13 -0400

    clean up formatting

--- lesson_5.js
+++ lesson_5.js
@@ -1,16 +1,15 @@
 var fs = require('fs');
-  var path = require('path');
+var path = require('path');
 
-  var folder = process.argv[2];
-  var ext = '.' + process.argv[3];
+var folder = process.argv[2];
+var ext = '.' + process.argv[3];
 
-  fs.readdir(folder, function (err, list) {
-    if (err) return console.error(err);
+fs.readdir(folder, function (err, list) {
+  if (err) return console.error(err);
 
-    for (i = 0; i < list.length; i++)
+  for (i = 0; i < list.length; i++)
 
-      if (path.extname(list[i]) === ext) {
-        console.log(list[i]);
-      }
-
-  });
+    if (path.extname(list[i]) === ext) {
+      console.log(list[i]);
+    }
+});
